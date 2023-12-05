7df44c7cd362072fee1fecb4f5e2fe5e48ac40fb
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-12-05 16:53:32 -0500

    Funcion tridimensional aplicada

--- matriz_tridimensional.c
+++ matriz_tridimensional.c
@@ -16,10 +16,36 @@ int MatrizTridimensional(int a, int b, int c, int mat[a][b][c]){
         }
         
     }
-    
+    return mat[a][b][c];
 }
 
 void main(){
-    int k, m, n;
+
+    int o, m, n;
+
+    printf("Ingresar el numero de matrices: ");
+    scanf("%d", &o);
+    printf("Ingresar el numero de filas: ");
+    scanf("%d", &n);
+    printf("Ingresar el numero de columnas: ");
+    scanf("%d", &m);
+
+    int mat[o][m][n];
+    mat[o][m][n]=MatrizTridimensional(o, m, n, mat);
+    
+    for (int k = 0; k < o; k++)
+    {
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            printf("\n");
+            for (int i = 0; i < n; i++)
+            {
+                printf("%d ", mat[k][j][i]);
+            }
+            
+        }
+        
+    }
     
 }
