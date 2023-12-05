3a0be60dd1846a54dd08b0471722018388766158
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-12-05 12:31:41 -0500

    Funcion matriz tridimencional

---
+++ matriz_tridimensional.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int MatrizTridimensional(int a, int b, int c, int mat[a][b][c]){
+    srand(time(NULL));
+    for (int k = 0; k < a; k++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            for (int i = 0; i < c; i++)
+            {
+                mat[k][j][i]=rand()%100+1;
+            }
+            
+        }
+        
+    }
+    
+}
+
+void main(){
+    
+}
