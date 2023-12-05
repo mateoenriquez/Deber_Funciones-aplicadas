9a00700227eb0585fb76d31cc194b86bf44b86a2
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-12-05 11:45:28 -0500

    Numeros aleatorios generados de entre 1 y 100

--- suma_matrices.c
+++ suma_matrices.c
@@ -17,5 +17,25 @@ int SumMatrices(int a, int b, int ma[a][b], int ma2[a][b], int res[a][b]){
 }
 
 void main(){
+    int m, n;
+    srand(time(NULL));
+    printf("Ingresar el numero de filas: ");
+    scanf("%d", &n);
+    printf("Ingresar el numero de columnas: ");
+    scanf("%d", &m);
+    int mat[n][m], mat2[n][m], resultado[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            mat[i][j]=rand()%100+1;
+            mat2[i][j]=rand()%100+1;
+
+        }
+        
+    }
     
+    
+
 }
